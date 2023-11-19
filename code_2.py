c8ffcdd4549e89ad8bac5795a395d6e61c948f50
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-11-19 20:22:22 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+
+    public static void main(String[] args) {
+    }
+        }
+        }
