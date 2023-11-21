c7b531421b00c4ceb8caf1200b7e0f92392272a4
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-11-21 08:24:16 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,13 +1,51 @@
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-}
+        }
+
             data[i] = (int) (Math.random() * 100) + 1;
-
         int[] data = new int[10];
+        }
         for (int i = 0; i < 10; i++) {
-
-    public static void main(String[] args) {
+public class RandomDataGenerator {
     }
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+}
+        for (int item : data) {
+
+
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    return data
+
+    main()
+        print(f"Random Number: {item}")
+    for item in data:
+def main():
+    data = generate_random_data()
+if __name__ == "__main__":
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+if __name__ == "__main__":
+
+def main():
+    main()
+
+    return data
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+def generate_random_data():}
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+
         }
+        for (int item : data) {
         }
+    }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
