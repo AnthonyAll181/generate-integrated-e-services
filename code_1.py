eca61f154792e4c77a5a9d61e6238a6d29efee46
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-11-27 08:31:20 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,14 @@
+    return data
+    for item in data:
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def generate_random_data():
+
+    main()
+
+def main():
+import random
