6c26adbe1b952a39366bcff4e638929e2a6e220f
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-12-04 20:39:49 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,24 +1,55 @@
-        }
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
+    for item in data:
+    data = generate_random_data()
+if __name__ == "__main__":
+    return data
+        print(f"Random Number: {item}")
 
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    main()
+
+
+import random
+
+def main():    }
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+        }
+        }
+
+}
+        int[] data = new int[10];
+public class RandomDataGenerator {
+        for (int item : data) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+if __name__ == "__main__":
+
+    main()
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    return data
+    for item in data:
+def generate_random_data():
+def main():
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        console.log("Random Number: " + item);
 }
 
-public class RandomDataGenerator {
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-        for (int i = 0; i < 10; i++) {
-        int[] data = new int[10];
-    }    }
-    public static void main(String[] args) {
+    for (let i = 0; i < 10; i++) {
+    return data;
+main();
+    });
+    data.forEach(item => {
 
-        for (int i = 0; i < 10; i++) {
-        int[] data = new int[10];
-public class RandomDataGenerator {
-        }
-            System.out.println("Random Number: " + item);
+    const data = [];
+    const data = generateRandomData();
 }
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
+function generateRandomData() {
+    }
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
