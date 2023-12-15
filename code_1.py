b0be9c71473f9e910640bd6f0301647724b44d15
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-12-15 08:51:51 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,54 +1,43 @@
-        int[] data = new int[10];
-        for (int item : data) {
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-
-            data[i] = (int) (Math.random() * 100) + 1;
-public class RandomDataGenerator {
-        }
-    public static void main(String[] args) {
-
-    }
-}    for (let i = 0; i < 10; i++) {
-}
-    });
-        console.log("Random Number: " + item);
-main();
-function main() {
-}
-
-
     const data = generateRandomData();
-    const data = [];
-        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+    });
+    for (let i = 0; i < 10; i++) {
     return data;
-    }
+        console.log("Random Number: " + item);
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
     data.forEach(item => {
+main();
 
-function generateRandomData() {    data = generate_random_data()
 
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
+function generateRandomData() {
+}
+    const data = [];
+    }
 def generate_random_data():
-    return data
-import random
 
-def main():
+        print(f"Random Number: {item}")
     main()
+    data = generate_random_data()
 
-
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-def generate_random_data():
-import random
 def main():
-        print(f"Random Number: {item}")
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+if __name__ == "__main__":
+
+import random
+if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
 
-    data = generate_random_data()
+def generate_random_data():
     for item in data:
+
+import random
+    data = generate_random_data()
     return data
 
-if __name__ == "__main__":
+def main():
+        print(f"Random Number: {item}")
+
     main()
