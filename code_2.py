feb234c7e8f04f071e11795a2caa22fc164a121f
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-12-21 08:58:41 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,30 @@
+    const data = generateRandomData();
+
+main();
+function generateRandomData() {
+
+    });
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+}
+        console.log("Random Number: " + item);
+    return data;
+    data.forEach(item => {
+
+    }
+    for (let i = 0; i < 10; i++) {
+    const data = [];    main()
+def generate_random_data():
     data = generate_random_data()
 
-        print(f"Random Number: {item}")
 
+
+    for item in data:
 def main():
 
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-    for item in data:
-    return data
 if __name__ == "__main__":
-
 import random
-def generate_random_data():
+        print(f"Random Number: {item}")
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
